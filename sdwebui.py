73403b4d2f6703fc4340477e69c483968427e8f7
hz109919 <138859400+hz109919@users.noreply.github.com>
2023-09-12 17:56:03 +0800

    Add files via upload

--- sdwebui.py
+++ sdwebui.py
@@ -3,6 +3,8 @@
 import io
 import json
 import os
+import threading
+import time
 
 import webuiapi
 import langid
@@ -17,6 +19,9 @@ from plugins import *
 
 @plugins.register(name="sdwebui", desc="利用stable-diffusion webui来画图", version="2.1", author="lanvent")
 class SDWebUI(Plugin):
+    create_image_list = []
+    isCreate = False
+
     def __init__(self):
         super().__init__()
         curdir = os.path.dirname(__file__)
@@ -34,11 +39,13 @@ class SDWebUI(Plugin):
             logger.info("[SD] inited")
         except Exception as e:
             if isinstance(e, FileNotFoundError):
-                logger.warn(f"[SD] init failed, {config_path} not found, ignore or see https://github.com/zhayujie/chatgpt-on-wechat/tree/master/plugins/sdwebui .")
+                logger.warn(
+                    f"[SD] init failed, {config_path} not found, ignore or see https://github.com/zhayujie/chatgpt-on-wechat/tree/master/plugins/sdwebui .")
             else:
-                logger.warn("[SD] init failed, ignore or see https://github.com/zhayujie/chatgpt-on-wechat/tree/master/plugins/sdwebui .")
+                logger.warn(
+                    "[SD] init failed, ignore or see https://github.com/zhayujie/chatgpt-on-wechat/tree/master/plugins/sdwebui .")
             raise e
-    
+
     def on_handle_context(self, e_context: EventContext):
 
         if e_context['context'].type != ContextType.IMAGE_CREATE:
@@ -47,10 +54,57 @@ class SDWebUI(Plugin):
         if ReplyType.IMAGE in channel.NOT_SUPPORT_REPLYTYPE:
             return
 
-        logger.debug("[SD] on_handle_context. content: %s" %e_context['context'].content)
+        reply = Reply()
+        reply.type = ReplyType.TEXT
+        if not self.isCreate:
+            self.isCreate = True
+            self.create_image(e_context)
+            reply.content = f"您的作品正在生成中，预计1~10分钟内完成，请耐心等待"
+        else:
+            self.create_image_list.append(e_context)
+            reply.content = f"您的作品正在排队中，当前位于第{len(self.create_image_list)}位，请耐心等待"
+
+        # 回复提示消息
+        e_context["reply"] = reply
+        e_context.action = EventAction.BREAK_PASS
+
+    def create_image(self, e_context: EventContext):
+        """
+            异步绘图
+        """
+        threading.Thread(target=self.__create_image, args=(Reply(), e_context)).start()
+
+    # def _create_image(self, e_context: EventContext):
+    #     logger.info("线程创建成功")
+    #     try:
+    #         self.__create_image(e_context)
+    #     except Exception as e:
+    #         logger.error("绘图发生错误:", e)
+    #         reply = Reply()
+    #         reply.type = ReplyType.TEXT
+    #         prefix = ""
+    #         if e_context["context"]["msg"].is_group:
+    #             prefix = "@" + e_context["context"]["msg"].actual_user_nickname + " "
+    #         reply.content = prefix + "您的作品生成发生错误, 请稍后再试"
+    #         channel = e_context["channel"]
+    #         _send(channel, reply, e_context["context"])
+    #
+    #         # 处理队列任务
+    #         if len(self.create_image_list) > 0:
+    #             self.create_image(self.create_image_list.pop(0))
+    #         else:
+    #             self.isCreate = False
+
+    def __create_image(self, reply: Reply(), e_context: EventContext):
+        """
+            使用stable diffusion接口绘图
+        """
+        logger.debug("[SD] on_handle_context. content: %s" % e_context['context'].content)
 
         logger.info("[SD] image_query={}".format(e_context['context'].content))
         reply = Reply()
+        old_prompt = ""
+        new_prompt = ""
         try:
             content = e_context['context'].content[:]
             # 解析用户输入 如"横版 高清 二次元:cat"
@@ -64,7 +118,7 @@ class SDWebUI(Plugin):
             unused_keywords = []
             if "help" in keywords or "帮助" in keywords:
                 reply.type = ReplyType.INFO
-                reply.content = self.get_help_text(verbose = True)
+                reply.content = self.get_help_text(verbose=True)
             else:
                 rule_params = {}
                 rule_options = {}
@@ -82,7 +136,7 @@ class SDWebUI(Plugin):
                     if not matched:
                         unused_keywords.append(keyword)
                         logger.info("[SD] keyword not matched: %s" % keyword)
-                
+
                 params = {**self.default_params, **rule_params}
                 options = {**self.default_options, **rule_options}
                 params["prompt"] = params.get("prompt", "")
@@ -91,12 +145,15 @@ class SDWebUI(Plugin):
                         prompt += f", {', '.join(unused_keywords)}"
                     else:
                         prompt = ', '.join(unused_keywords)
+                old_prompt = prompt
+                new_prompt = prompt
                 if prompt:
                     lang = langid.classify(prompt)[0]
                     if lang != "en":
                         logger.info("[SD] translate prompt from {} to en".format(lang))
                         try:
-                            prompt = Bridge().fetch_translate(prompt, to_lang= "en")
+                            prompt = Bridge().fetch_translate(prompt, to_lang="en")
+                            new_prompt = prompt
                         except Exception as e:
                             logger.info("[SD] translate failed: {}".format(e))
                         logger.info("[SD] translated prompt={}".format(prompt))
@@ -113,15 +170,35 @@ class SDWebUI(Plugin):
                 result.image.save(b_img, format="PNG")
                 reply.content = b_img
             e_context.action = EventAction.BREAK_PASS  # 事件结束后，跳过处理context的默认逻辑
+
+            # 发送提醒消息
+            text_reply = Reply()
+            text_reply.type = ReplyType.TEXT
+            prefix = ""
+            if e_context["context"]["msg"].is_group:
+                prefix = "@" + e_context["context"]["msg"].actual_user_nickname + " "
+            text_reply.content = prefix + f"您的作品生成完成,初始prompt:'{old_prompt}',转换后prompt:'{new_prompt}'"
+            channel = e_context["channel"]
+            _send(channel, text_reply, e_context["context"])
         except Exception as e:
             reply.type = ReplyType.ERROR
-            reply.content = "[SD] "+str(e)
+            reply.content = "[SD] " + str(e)
             logger.error("[SD] exception: %s" % e)
             e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
         finally:
             e_context['reply'] = reply
 
-    def get_help_text(self, verbose = False, **kwargs):
+        # 发送消息
+        channel = e_context["channel"]
+        _send(channel, reply, e_context["context"])
+
+        # 处理队列任务
+        if len(self.create_image_list) > 0:
+            self.create_image(self.create_image_list.pop(0))
+        else:
+            self.isCreate = False
+
+    def get_help_text(self, verbose=False, **kwargs):
         if not conf().get('image_create_prefix'):
             return "画图功能未启用"
         else:
@@ -129,7 +206,7 @@ class SDWebUI(Plugin):
         help_text = "利用stable-diffusion来画图。\n"
         if not verbose:
             return help_text
-        
+
         help_text += f"使用方法:\n使用\"{trigger}[关键词1] [关键词2]...:提示语\"的格式作画，如\"{trigger}横版 高清:cat\"\n"
         help_text += "目前可用关键词：\n"
         for rule in self.rules:
@@ -140,3 +217,19 @@ class SDWebUI(Plugin):
             else:
                 help_text += "\n"
         return help_text
+
+
+def _send(channel, reply: Reply, context, retry_cnt=0):
+    """
+        使用channel发送wx消息
+    """
+    try:
+        channel.send(reply, context)
+    except Exception as e:
+        logger.error("[WX] sendMsg error: {}".format(str(e)))
+        if isinstance(e, NotImplementedError):
+            return
+        logger.exception(e)
+        if retry_cnt < 2:
+            time.sleep(3 + 3 * retry_cnt)
+            channel.send(reply, context, retry_cnt + 1)
